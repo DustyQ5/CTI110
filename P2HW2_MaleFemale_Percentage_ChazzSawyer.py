4cd67182c69f534be96724be5312fbf01b3c1ebc
DustyQ5 <43518797+DustyQ5@users.noreply.github.com>
2018-10-30 10:01:23 -0400

    Add files via upload

---
+++ P2HW2_MaleFemale_Percentage_ChazzSawyer.py
@@ -0,0 +1,19 @@
+# Program to calculate female and male percentage of a given size of a total
+#number of students
+# 10/30/2018
+# P2HW2_MaleFemale_Percentage=
+# CHAZZ SAWYER
+#
+
+# asks for total number of students and how many are female ane male
+total_number_students = int(input ('How many students in total?'))
+number_females = float ( input ('How students many are females?'))
+number_males = float(input('How many students are male?'))
+#prints percentage of male and female
+print ('Female percentage:', number_females / total_number_students * 100)
+print ('Male percentage:', number_males / total_number_students * 100)
+
+
+
+
+                     
